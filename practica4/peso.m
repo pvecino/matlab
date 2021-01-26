9cd7459a4fa8d59aec19ffba62bc3a15a4bab106
pvecino <pvecino@users.noreply.github.com>
2021-01-26 14:40:45 +0100

    Delete practica4 directory

--- practica4/peso.m
+++
@@ -1,9 +0,0 @@
-function y = peso(n, snr, var)
-  
-nueva_desv = sqrt(var);  
-fdp_ruido = normpdf(n, 0, sqrt(1/(2*snr)));
-fdp_ruido_estrella = normpdf(n, 0, nueva_desv);
-
-y = (fdp_ruido./fdp_ruido_estrella)';
- 
- 
